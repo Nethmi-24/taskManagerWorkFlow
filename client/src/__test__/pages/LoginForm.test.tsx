f1314b60e368697ed2487f680168a869cc54be9b
Nethmi-24 <batawalann.20@uom.lk>
2024-03-17 23:58:23 +0530

    commit on branch 5 failed

--- client/src/__test__/pages/LoginForm.test.tsx
+++ client/src/__test__/pages/LoginForm.test.tsx
@@ -220,7 +220,7 @@ test("navigation", async () => {
 
   //check sign up page
   await waitFor(() => {
-    const aboutUsText = screen.getByText(/Already Have an Account/i);
+    const aboutUsText = screen.getByText(/nothing/i);
     expect(aboutUsText).toBeVisible();
   });
 });
